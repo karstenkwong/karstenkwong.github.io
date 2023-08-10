e872a1a32c7f8e22f87ba385c2119e133daed096
Karsten <karstenkwong@gmail.com>
2023-08-10 18:19:53 +0800

    Test

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  "scripts": {
+    "build": "vite build",
+    "preview": "vite preview"
   }
 })
